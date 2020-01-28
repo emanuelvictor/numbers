73b87532549f712eaf6452ae9096fa5cc2f38c85
Emanuel Victor <emanuel.fonseca@pti.org.br>
2020-01-28 11:39:36 -0300

    Navigation inserted

--- src/main/java/br/com/produtec/numbers/application/managedbeans/NumbersManagedBean.java
+++ src/main/java/br/com/produtec/numbers/application/managedbeans/NumbersManagedBean.java
@@ -5,10 +5,11 @@ import br.com.produtec.numbers.domain.entity.numbers.Number;
 import br.com.produtec.numbers.domain.service.NumbersService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 
 import javax.annotation.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 @ViewScoped
@@ -34,9 +35,38 @@ public class NumbersManagedBean {
     private Number number = new Number();
 
     /**
+     * @param id
+     */
+    public void initialize(long id) {
+        if (!FacesContext.getCurrentInstance().isPostback()) {
+            this.number = this.numbersService.findById(id);
+        }
+    }
+
+    /**
      *
      */
-    public Number save() {
-        return numbersService.save(number);
+    public void save() {
+        numbersService.save(number);
+        changeToDetail(number.getId());
     }
+
+    /**
+     *
+     */
+    private void changeToDetail(final long id) {
+        try {
+            final String url = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/number-detail.jsf?numberId=" + id;
+            FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+        } catch (IOException ignored) {
+        }
+    }
+
+    /**
+     *
+     */
+    public String changeToInsert() {
+        return FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/numbers.jsf?faces-redirect=true";
+    }
+
 }
