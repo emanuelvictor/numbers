ba8b73495122a5d73f3d9d8b02e1aa7941e7b227
Emanuel Victor <emanuel.fonseca@pti.org.br>
2020-01-29 08:52:30 -0300

    Oracle working

--- src/main/resources/db/migration/20190715__RELEASE.sql
+++ src/main/resources/db/migration/20190715__RELEASE.sql
@@ -19,9 +19,10 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
-create table public.number (id  bigserial not null, created timestamp not null, updated timestamp, digit int2 not null, processing_time int4 not null, primary key (id));
-create table public.number_audited (id int8 not null, revision int8 not null, revision_type int2, digit int2, processing_time int4, primary key (id, revision));
-create table public.revision (id  bigserial not null, timestamp int8 not null, user_id int8, primary key (id));
-alter table public.number drop constraint if exists UK_number_digit;
-alter table public.number add constraint UK_number_digit unique (digit);
-alter table public.number_audited add constraint FK_number_audited_revision foreign key (revision) references public.revision;
+create table numbers (id number(19,0) not null, created timestamp not null, updated timestamp, digit number(5,0) not null check (digit<=100 AND digit>=1), processing_time number(10,0) not null, primary key (id));
+create table numbers_audited (id number(19,0) not null, revision number(19,0) not null, revision_type number(3,0), digit number(5,0), processing_time number(10,0), primary key (id, revision));
+create table revision (id number(19,0) not null, timestamp number(19,0) not null, user_id number(19,0), primary key (id));
+alter table numbers drop constraint UK_numbers_digit;
+alter table numbers add constraint UK_numbers_digit unique (digit);
+create sequence hibernate_sequence start with 1 increment by  1;
+alter table numbers_audited add constraint FKjm9qhqq9lxo2hli5lj1g92r6f foreign key (revision) references revision;
